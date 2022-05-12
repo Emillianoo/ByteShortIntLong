30456fdc06bec5fb2c8117e0bd36ea40eb4ff095
EmillianoPC <emil090596@gmail.com>
2022-05-12 23:12:52 +0200

    Adding MAX and MIN for byte, short and long

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,21 @@ public class Main {
         //Overflow and Underflow
         System.out.println("Busted MAX value = " + (myMaxValue + 1));
         System.out.println("Busted MIN value = " + (myMinValue + 1));
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Values = " + myMinByteValue);
+        System.out.println("Byte Maximum Values = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Values = " + myMinShortValue);
+        System.out.println("Short Maximum Values = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Values = " + myMinLongValue);
+        System.out.println("Long Maximum Values = " + myMaxLongValue);
     }
 }
